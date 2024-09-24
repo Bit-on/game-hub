ba05b889be838fa60ca060d9814e7d2623d2e2cc
Abul Hasnat <31097994+Bit-on@users.noreply.github.com>
2024-09-24 15:12:02 +0100

    Setup routhing

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,44 @@
+import { Box, Flex, Grid, GridItem } from "@chakra-ui/react";
+import GenreList from "../components/GenreList";
+import GameHeading from "../components/GameHeading";
+import PlatformSelector from "../components/PlatformSelector";
+import SortSelector from "../components/SortSelector";
+import GameGrid from "../components/GameGrid";
+
+const HomePage = () => {
+  return (
+    <Grid
+      templateAreas={{
+        // base: `"nav" "main"`,
+        // lg: `"nav nav" "aside main"`,
+        base: `"main"`,
+        lg: `"aside main"`,
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr",
+      }}
+    >
+      {/* <GridItem area="nav">
+      <NavBar />
+    </GridItem> */}
+      <GridItem area="aside" padding={5}>
+        <GenreList />
+      </GridItem>
+      <GridItem area="main">
+        <Box paddingLeft={2}>
+          <GameHeading />
+          <Flex marginBottom={5}>
+            <Box marginRight={5}>
+              <PlatformSelector />
+            </Box>
+            <SortSelector />
+          </Flex>
+        </Box>
+        <GameGrid />
+      </GridItem>
+    </Grid>
+  );
+};
+
+export default HomePage;
